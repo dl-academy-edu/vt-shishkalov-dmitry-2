c12d9ff2555ebc76b661046ed39edf88121fae1c
Dmitrii64 <60173944+Dmitrii64@users.noreply.github.com>
2020-06-09 14:33:47 +0400

    Проект
    Доделал итоговую работу,  постарался соответствовать критериям, но есть несколько вопросов по вещам, которых нет в  критериях, но хотелось бы их понять для себя:
    1) Сетку для карточек. Я её сделал (net.scss), но она у меня не работала для страницы catalog, пришлось сделать адаптив немного извращенский. В критериях сетки нет, но интересно, что не так.
    2) Когда я ужимаю экран в браузере, всё отображается корректно, как по макету, но когда я выложил его в интернет через GitHub Pages, некоторые детали отображались неправильно (не подгружаются некоторые картинки (особенно на странице animation scene); в футере поехали ссылки и из-за них полосы тоже сместились; сайт можно немного проскроллить влево как будто лишний скрытый отступ справа есть; если повернуть экран телефона вылезает мобильное меню само хотя "слушаю" я click). Вопрос: почему в браузере всё работает корректно, а на практике - нет, и как это исправить, чтобы потом показать было не стыдно? Но в целом проект я сделал, понимаю, что в нём много небольших недоработок, но для первого опыта, надеюсь, неплохо. Оцени, пожалуйста, на сколько это примерно баллов?)

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -20,3 +20,16 @@ window.addEventListener("keydown", function (event) {
     buttonOpen.focus();
   }
 })
+
+
+var mobileHeader = document.querySelector(".mobile-header_js");
+var buttonOpenHeader = document.querySelector(".header__menu_js");
+var buttonCloseHeader = document.querySelector(".mobile-header__button_js");
+
+buttonOpenHeader.addEventListener("click", function () {
+  mobileHeader.classList.add("mobile-header_open");
+});
+
+buttonCloseHeader.addEventListener("click", function () {
+  mobileHeader.classList.remove("mobile-header_open");
+});
